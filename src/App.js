0dde4c33d1113ee300a4b7a540823bd79f1af1c2
a-yh <103937002+a-yh@users.noreply.github.com>
2023-08-30 14:07:19 -0400

    remove other routes

--- src/App.js
+++ src/App.js
@@ -1,48 +1,15 @@
 import "./App.css";
-import { EasybaseProvider } from "easybase-react";
-import ebconfig from "./ebconfig";
-import HeightContainer from "./HeightContainer";
-import Keyword from "./Keyword";
-import MetContainer from "./MetContainer";
-import MtaContainer from "./MtaContainer";
-import ActorContainer from "./ActorContainer";
-import PlayerContainer from "./PlayerContainer";
-import { HashRouter, Route, Switch, Link } from "react-router-dom";
-import Home from "./Home";
+
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Score from "./javascript/components/Home";
 
 const App = () => {
   return (
-    <EasybaseProvider ebconfig={ebconfig}>
-      <HashRouter>
-        <Switch>
-          <Route path="/mj">
-            <Score />
-          </Route>
-          <Route path="/height">
-            <HeightContainer />
-          </Route>
-          <Route path="/keyword">
-            <Keyword />
-          </Route>
-          <Route path="/art">
-            <MetContainer />
-          </Route>
-          <Route path="/mta">
-            <MtaContainer />
-          </Route>
-          <Route path="/actor">
-            <ActorContainer />
-          </Route>
-          <Route path="/wsl">
-            <PlayerContainer />
-          </Route>
-          <Route path="/">
-            <Score />
-          </Route>
-        </Switch>
-      </HashRouter>
-    </EasybaseProvider>
+    <HashRouter>
+      <Switch>
+        <Route path="/" exact component={Score} />
+      </Switch>
+    </HashRouter>
   );
 };
 
